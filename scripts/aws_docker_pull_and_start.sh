4b902023a8475da2190769dd98e4d68709dc14e1
NUWAN SENARATNA <nuwan.senaratna@NUWANS-MacBookPro-2018.local>
2021-06-05 14:36:25 +0530

    Added AWS scripts

---
+++ scripts/aws_docker_pull_and_start.sh
@@ -0,0 +1,7 @@
+echo "AWS_EC2_IP_ADDRESS=$AWS_EC2_IP_ADDRESS"
+echo "AWS_EC2_PEM_FILE=$AWS_EC2_PEM_FILE"
+echo "AWS_EC2_USER=$AWS_EC2_USER"
+
+EC2_SUDO="ssh -i $AWS_EC2_PEM_FILE $AWS_EC2_USER@$AWS_EC2_IP_ADDRESS sudo "
+
+$EC2_SUDO docker run -p 81:4001 -d nuuuwan/gig_server &
